080b613f978d9572fa712907c9433baba0fecae7
Linus Dietz <dietli@tribus-it.local>
2021-02-18 08:35:21 +0100

    Run as Service // Add Logging // Use Config.yaml

--- netatmoMQTTBridge.py
+++ netatmoMQTTBridge.py
@@ -2,35 +2,66 @@ import pyatmo
 import json
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
+import datetime
+import time
+from datetime import timedelta  
+import yaml
 
-CLIENT_ID = '<dev.netatmo - clientID>'
-CLIENT_SECRET = '<dev.netatmo - clientSecret>'
-USERNAME = '<netatmo e-mail>'
-PASSWORD = '<netatmo - password>'
-MACADRESS = '<homecoach macAdress>'
-mqttBrokerIP = '<mqtt broker ip or fqdn>'
 
 def main():
-    airData = getNetatmoData()
-    sendToHA(airData)
+
+    # Load cfg
+    cfg = loadConf()
+    
+    # Initial Update on startup
+    airData = getNetatmoData(cfg)
+    sendToHA(airData, cfg)
+    nextFetchSec = getNextFetchTime (datetime.datetime.fromtimestamp(airData['lastUpdate'][0]))
+    
+    while True:
+        time.sleep(nextFetchSec)
+        airData = getNetatmoData(cfg)
+        sendToHA(airData, cfg)
+        nextFetchSec = getNextFetchTime (datetime.datetime.fromtimestamp(airData['lastUpdate'][0]))
+
+        
+def getNextFetchTime (lastUpdate):
+    
+    now = datetime.datetime.today()
+    logToConsole (f"Last update from Netatmo was {lastUpdate}")
+
+    nextFetch = lastUpdate + timedelta(minutes=5, seconds=15)
+    if (nextFetch < now):
+        nextFetch = nextFetch + timedelta(minutes=5)
+    
+    nextFetchSec = (nextFetch - now).seconds
+
+    logToConsole (f"Wait for {nextFetchSec} seconds until next fetch at {nextFetch}...")
+
+    return nextFetchSec
 
 
-def getNetatmoData ():
+def getNetatmoData (cfg):
+    
     authorization = pyatmo.ClientAuth(
-    client_id=CLIENT_ID,
-    client_secret=CLIENT_SECRET,
-    username=USERNAME,
-    password=PASSWORD,
+    client_id= cfg['netatmoMQTTBridge']['CLIENT_ID'],
+    client_secret=cfg['netatmoMQTTBridge']['CLIENT_SECRET'],
+    username=cfg['netatmoMQTTBridge']['USERNAME'],
+    password=cfg['netatmoMQTTBridge']['PASSWORD'],
     scope="read_homecoach",
     )
 
     airData = pyatmo.HomeCoachData(authorization)
 
+    logToConsole ("Fetching data from Netatmo cloud...")
+
+    MACADRESS = cfg['netatmoMQTTBridge']['MACADRESS']
     raw = airData.raw_data
     data = airData.get_last_data(MACADRESS)
 
     airData = {}
 
+    airData['lastUpdate'] = data[MACADRESS]['When'], "", ""
     airData['Temperature'] = data[MACADRESS]['Temperature'], "mdi:thermometer", "°C"
     airData['Humidity'] = data[MACADRESS]['Humidity'], "hass:water-percent", "%"
     airData['CO2'] = data[MACADRESS]['CO2'], "mdi:molecule-co2", "ppm"
@@ -44,8 +75,10 @@ def getNetatmoData ():
 
     return airData
 
-def sendToHA (airData):
+def sendToHA (airData, cfg):
     for key, value in airData.items():
+
+        mqttBrokerIP = cfg['netatmoMQTTBridge']['mqttBrokerIP']
 
         payload = {}
         payload['name'] = f"netatmoMQTTBridge - {key}"
@@ -66,6 +99,9 @@ def sendToHA (airData):
                 port=1883, client_id="netatmoMQTTBridge", keepalive=60, will=None, 
                 tls=None, protocol=mqtt.MQTTv311, transport="tcp")
 
+def logToConsole (text):
+    now = datetime.datetime.today()
+    print (f"{now} - {text}")
 
 def getWifiStatus (wifi_status):
     if (wifi_status <= 56):
@@ -90,5 +126,10 @@ def getHealthIdx (health_idx):
         health_idx = "Unhealthy"
 
     return health_idx
+
+def loadConf ():
+    with open("config.yaml", "r") as ymlfile:
+        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+    return cfg
 
 main()
